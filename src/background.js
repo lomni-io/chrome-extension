f0563d6e16c107df0b292104d2427479445f8a06
Daniel Kanczuk <danielsussa@gmail.com>
2023-01-16 23:13:19 -0300

    Rc/0.9.0 (#1)

--- src/background.js
+++ src/background.js
@@ -1,20 +1,9 @@
-let activeTab = null
 
 chrome.runtime.onConnectExternal.addListener(function(port) {
     console.log("onConnectExternal", port)
 
     chrome.tabs.onActivated.addListener((info) =>{
-        activeTab = info.tabId
-        console.log("onActivated", info)
-        chrome.tabs.get(info.tabId,(tab => {
-            port.postMessage({
-                kind: 'tab-change-response',
-                data: tab
-            });
-            console.log("info: ", tab)
-        }))
-
-        chrome.tabs.query({currentWindow: true}, tabs => {
+        chrome.tabs.query({}, tabs => {
             port.postMessage({
                 kind: 'all-tabs-response',
                 data: tabs
@@ -24,7 +13,7 @@ chrome.runtime.onConnectExternal.addListener(function(port) {
     });
 
     chrome.tabs.onRemoved.addListener((tabId, removedInfo) =>{
-        chrome.tabs.query({currentWindow: true}, tabs => {
+        chrome.tabs.query({}, tabs => {
             port.postMessage({
                 kind: 'all-tabs-response',
                 data: tabs
@@ -33,31 +22,49 @@ chrome.runtime.onConnectExternal.addListener(function(port) {
     })
 
     chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) =>{
-        if (changeInfo.title || changeInfo.status === 'complete'){
-            chrome.tabs.query({currentWindow: true}, tabs => {
-                port.postMessage({
-                    kind: 'all-tabs-response',
-                    data: tabs
-                });
+        chrome.tabs.query({}, tabs => {
+            port.postMessage({
+                kind: 'all-tabs-response',
+                data: tabs
             });
-        }
-        if (changeInfo.title || changeInfo.status === 'complete' && tab.active){
-            chrome.tabs.get(tabId,(tab => {
-                console.log('tab-change-response', tab)
-                port.postMessage({
-                    kind: 'tab-change-response',
-                    data: tab
-                });
-            }))
-        }
+        });
         console.log("onUpdated", tabId, changeInfo, tabId)
+    })
+
+    chrome.tabs.onMoved.addListener((tabId, changeInfo) =>{
+        chrome.tabs.query({}, tabs => {
+            port.postMessage({
+                kind: 'all-tabs-response',
+                data: tabs
+            });
+        });
+    })
+
+
+    chrome.tabGroups.onUpdated.addListener(group =>{
+        chrome.tabGroups.query({}, groups => {
+            port.postMessage({
+                kind: 'all-tab-groups-response',
+                data: groups
+            });
+        });
+        console.log("onUpdatedGroup", group)
+    })
+
+    chrome.tabGroups.onMoved.addListener(group =>{
+        chrome.tabs.query({}, tabs => {
+            port.postMessage({
+                kind: 'all-tabs-response',
+                data: tabs
+            });
+        });
     })
 
     port.onMessage.addListener(function(msg) {
         console.log('new message:: ', msg)
 
         if (msg.kind === 'all-tabs-request') {
-            chrome.tabs.query({currentWindow: true}, tabs => {
+            chrome.tabs.query({}, tabs => {
                 port.postMessage({
                     kind: 'all-tabs-response',
                     data: tabs
@@ -65,31 +72,61 @@ chrome.runtime.onConnectExternal.addListener(function(port) {
                 console.log('all-tabs-request:: ', tabs)
             });
         }
+        if (msg.kind === 'all-tab-groups-request') {
+            chrome.tabGroups.query({}, groups => {
+                port.postMessage({
+                    kind: 'all-tab-groups-response',
+                    data: groups
+                });
+                console.log('all-tab-groups-response:: ', groups)
+            });
+        }
+        if (msg.kind === 'collapse-tab-groups') {
+            chrome.tabGroups.update(msg.group, {collapsed: msg.collapse})
+        }
+        if (msg.kind === 'title-tab-groups') {
+            chrome.tabGroups.update(msg.group, {title: msg.title})
+        }
+        if (msg.kind === 'color-tab-groups') {
+            chrome.tabGroups.update(msg.group, {color: msg.color})
+        }
         if (msg.kind === 'open-request-existing-tab') {
             chrome.tabs.update(msg.tab, {active: true})
         }
-        if (msg.kind === 'open-request-new-tab') {
-            chrome.tabs.create({
-                url: msg.url
+        if (msg.kind === 'pin-tab') {
+            chrome.tabs.update(msg.tab, {pinned: true})
+        }
+        if (msg.kind === 'close-tabs') {
+            chrome.tabs.remove(msg.tab)
+        }
+        if (msg.kind === 'move-tab') {
+            chrome.tabs.update(msg.tab, {pinned: false}, () => {
+                if (msg.groupId === -1){
+                    chrome.tabs.ungroup(msg.tab)
+                }else{
+                    chrome.tabs.group({tabIds: msg.tab, groupId: msg.groupId})
+                }
+                chrome.tabs.move(msg.tab, {windowId: msg.windowId, index: msg.index})
+            })
+        }
+        if (msg.kind === 'group-tabs') {
+            chrome.tabs.group({tabIds: msg.tabs, groupId: msg.groupId})
+        }
+        if (msg.kind === 'ungroup-tabs') {
+            chrome.tabs.ungroup(msg.tabs)
+        }
+        if (msg.kind === 'unpin-tab') {
+            chrome.tabs.update(msg.tab, {pinned: false})
+        }
+        if (msg.kind === 'open-and-update') {
+            chrome.tabs.create({url: msg.url, windowId: msg.windowId, active: msg.active, index: msg.index}, tab => {
+                if (msg.groupId > -1){
+                    chrome.tabs.group({tabIds: tab.id, groupId: msg.groupId})
+                }
             });
         }
-        if (msg.kind === 'open-request') {
-            chrome.tabs.query({currentWindow: true, url: msg.data.url}, tabs => {
-                if (tabs.length > 0){
-                    chrome.tabs.update(tabs[0].id, {active: true})
-                }else{
-                    if (msg.data.option === 'new'){
-                        chrome.tabs.create({
-                            url: msg.data.url
-                        });
-                    }else{
-                        chrome.tabs.query({currentWindow: true, active: true}, tabs => {
-                            chrome.tabs.update(tabs[0].id, {url: msg.data.url})
-                        })
-                    }
-
-                }
-            })
+        if (msg.kind === 'open-request-new-tab') {
+            chrome.tabs.create({url: msg.url, pinned: msg.pinned, active: msg.active});
         }
     })
 })
